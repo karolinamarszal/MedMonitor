c64b5982c87fbab2d6f5a70b347f9cda42a77773
Karolina Marszal <karomarszal@gmail.com>
2023-03-17 17:42:51 +0100

    Created home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react"
+import Appointments from "../components/Appointments"
 
 
 const Home = () => {
 
   return (
     <div>
+      <Appointments />
       <h2>test</h2>
     </div>
   )
