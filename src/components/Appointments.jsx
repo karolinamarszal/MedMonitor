cf1f86c7c974d18a467f89256d939c912321c8d5
Karolina Marszal <karomarszal@gmail.com>
2023-03-23 13:26:13 +0100

    Created appointments component

---
+++ src/components/Appointments.jsx
@@ -0,0 +1,23 @@
+import React, { useState} from "react"
+import CreateNewAppointment from "./CreateNewAppointment"
+import AppointmentsTable from "./AppointmentsTable"
+import { AppointmentsContextProvider } from "../context/AppointmentsContext";
+
+
+const Appointments = () => {
+
+  return (
+    <AppointmentsContextProvider>
+      <div className="contentContainer">
+        <div>
+          <CreateNewAppointment />
+        </div>
+        <div>
+          <AppointmentsTable />
+        </div>
+      </div>
+    </AppointmentsContextProvider>
+  )
+}
+
+export default Appointments
