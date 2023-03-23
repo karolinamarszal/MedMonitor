5fbf61ae16a9bf9fc1aab6e7b6078896ca4f745a
Karolina Marszal <karomarszal@gmail.com>
2023-03-23 13:27:05 +0100

    Added functionality to create new appointment

--- src/components/CreateNewAppointment.jsx
+++ src/components/CreateNewAppointment.jsx
@@ -1,46 +1,92 @@
-import React, { useState } from "react"
+import React, { useState, useContext } from "react"
+import { FaBriefcaseMedical, FaBookMedical, FaFileMedical, FaCalendarAlt, FaPlus } from "react-icons/fa";
 import Modal from "./Modal"
+import CustomInput from "./CustomInput"
+import CustomTextarea from "./CustomTextarea"
+import { AppointmentsContext } from "../context/AppointmentsContext";
 
 const CreateAppointment = props => {
 
-  const [show, setShow] = useState(false)
+  const { unshiftToAppointments} = useContext(AppointmentsContext);
 
-  const [formData, setFormData] = useState({appointmentType: "", description: "", date: ""});
+  const [show, setShow] = useState(false)
+  
+  const initialFormData = {appointmentType: {value:"", error: null}, description: {value:"", error: null}, date: {value:"", error: null}, addFile: {value:"", error: null}};
+
+  const [formData, setFormData] = useState(initialFormData);
+
+  
 
   const handleChange = (e) => {
     const {name, value} = e.target;
-    setFormData((prevFormData) => ({ ...prevFormData, [name]: value}));
+    // console.log(name, value);
+    setFormData((prevFormData) => {
+      // return { ...prevFormData, [name]: {value}};
+      const obj = {...prevFormData};
+      // console.log(obj, prevFormData);
+      obj[name]['value'] = value;
+      // console.log(obj, obj[name], obj[name]["value"])
+      return obj;
+    });
   };
 
   const closeModal = () => {
     setShow(false);
   }
 
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    alert(`Appointment type: ${formData.appointmentType}, Description: ${formData.description}`);
+    console.log("hello")
+    if(!formData.appointmentType.value){
+      formData.appointmentType.error = "This field is required"
+    }
+    // alert(`Appointment type: ${formData.appointmentType.value}, Description: ${formData.description.value}, Date: ${formData.date.value}`);
+    unshiftToAppointments(formData);
     setShow(false);
+    setFormData(initialFormData);
   };
 
 
 
   return (
     <div>
-      <button onClick={() => setShow(true)}>Add appointment</button>
+      <div className="buttonContainer">
+        <button className="buttonAdd" onClick={() => setShow(true)}><i className="mr-400"><FaPlus/></i><span>Add appointment</span></button>
+      </div>
       <Modal title="Add new appointment" onClose={closeModal} onSubmit={handleSubmit} show={show}>
-        <form className="formContainer">
-            <div className="inputContainer mb-400">
-              <label htmlFor="appointmentType" className="label">Appointment type</label>
-              <input type="text" id="appointmentType" name="appointmentType" value={formData.appointmentType} onChange={handleChange}/>
-            </div>
-            <div className="inputContainer mb-400">
-              <label htmlFor="date" className="label">Date</label>
-              <input type="text" id="date" name="date" value={formData.date} onChange={handleChange} onFocus={(e)=>console.log(e)}/>
-            </div>
-            <div className="inputContainer">
-              <label className="label" htmlFor="description">Description</label>
-              <textarea id="description" name="description" value={formData.description} onChange={handleChange}/>
-            </div>
+        <form>
+            <CustomInput 
+              label="Appointment type" 
+              type="text" 
+              icon={<FaBriefcaseMedical/>}
+              name="appointmentType" 
+              value={formData.appointmentType.value} 
+              onChange={handleChange}
+            />
+            <CustomInput 
+              label="Date"
+              icon={<FaCalendarAlt />}
+              type="date"
+              name="date"
+              value={formData.date.value}
+              onChange={handleChange}
+            />
+            <CustomTextarea
+              label="Description" 
+              icon={<FaBookMedical/>}
+              name="description" 
+              value={formData.description.value} 
+              onChange={handleChange}
+            />
+            <CustomInput 
+              label="Add file" 
+              type="file" 
+              icon={<FaFileMedical/>}
+              name="addFile" 
+              value={formData.addFile.value} 
+              onChange={handleChange}
+            />
         </form>
       </Modal>
     </div>
