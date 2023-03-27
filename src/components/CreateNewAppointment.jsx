3b0ebc3682b5da93a417da207bf33fe7fbdf84dd
Karolina Marszal <karomarszal@gmail.com>
2023-03-27 13:21:13 +0200

    Added alert functionality

--- src/components/CreateNewAppointment.jsx
+++ src/components/CreateNewAppointment.jsx
@@ -4,16 +4,20 @@ import Modal from "./Modal"
 import CustomInput from "./CustomInput"
 import CustomTextarea from "./CustomTextarea"
 import { AppointmentsContext } from "../context/AppointmentsContext";
+import Alert from "./Alert"
 
 const CreateAppointment = () => {
 
   const {
+    appointments,
     showAppointmentForm, 
     setShowAppointmentForm, 
     formData, setFormData, 
     initialFormData, 
     handleSubmit,
     setEditIndex,
+    alert,
+    showAlert,
   } = useContext(AppointmentsContext);
 
   
@@ -33,12 +37,14 @@ const CreateAppointment = () => {
 
   const closeModal = () => {
     setShowAppointmentForm(false);
+    showAlert(false, "", "")
   }
 
 
   const handleNewAppointmentBtnClick = () => {
     setEditIndex(-1);
     setShowAppointmentForm(true);
+    showAlert(false, "", "")
     setFormData(initialFormData);
   }
 
@@ -47,41 +53,45 @@ const CreateAppointment = () => {
   return (
     <div>
       <div className="buttonContainer">
+        {alert.show && <Alert {...alert} removeAlert={showAlert} appointments={appointments}/>}
         <button className="buttonAdd" onClick={handleNewAppointmentBtnClick}><i className="mr-400"><FaPlus/></i><span>Add appointment</span></button>
       </div>
       <Modal title="Add new appointment" onClose={closeModal} onSubmit={handleSubmit} show={showAppointmentForm}>
         <form>
-            <CustomInput 
-              label="Appointment type" 
-              type="text" 
-              icon={<FaBriefcaseMedical/>}
-              name="appointmentType" 
-              value={formData.appointmentType.value} 
-              onChange={handleChange}
-            />
-            <CustomInput 
-              label="Date"
-              icon={<FaCalendarAlt />}
-              type="date"
-              name="date"
-              value={formData.date.value}
-              onChange={handleChange}
-            />
-            <CustomTextarea
-              label="Description" 
-              icon={<FaBookMedical/>}
-              name="description" 
-              value={formData.description.value} 
-              onChange={handleChange}
-            />
-            <CustomInput 
-              label="Add file" 
-              type="file" 
-              icon={<FaFileMedical/>}
-              name="addFile" 
-              value={formData.addFile.value} 
-              onChange={handleChange}
-            />
+          <div className="alertEmptyForm">
+            {alert.show && <Alert {...alert} removeAlert={showAlert} appointments={appointments}/>}
+          </div>
+          <CustomInput 
+            label="Appointment type" 
+            type="text" 
+            icon={<FaBriefcaseMedical/>}
+            name="appointmentType" 
+            value={formData.appointmentType.value} 
+            onChange={handleChange}
+          />
+          <CustomInput 
+            label="Date"
+            icon={<FaCalendarAlt />}
+            type="date"
+            name="date"
+            value={formData.date.value}
+            onChange={handleChange}
+          />
+          <CustomTextarea
+            label="Description" 
+            icon={<FaBookMedical/>}
+            name="description" 
+            value={formData.description.value} 
+            onChange={handleChange}
+          />
+          <CustomInput 
+            label="Add file" 
+            type="file" 
+            icon={<FaFileMedical/>}
+            name="addFile" 
+            value={formData.addFile.value} 
+            onChange={handleChange}
+          />
         </form>
       </Modal>
     </div>
