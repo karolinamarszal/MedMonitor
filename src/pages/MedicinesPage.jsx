6b0f8fde90349f0ddd5904025a722ec2618c6275
Karolina Marszal <karomarszal@gmail.com>
2023-03-29 11:17:26 +0200

    Created Medicines page

---
+++ src/pages/MedicinesPage.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Medicines from "../components/Medicines"
+
+const MedicinesPage = () => {
+  return (
+    <div className="container">
+      <Medicines />
+    </div>
+  );
+}
+
+export default MedicinesPage
