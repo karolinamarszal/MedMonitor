2605de67d7a1db220d826929293507470183ee25
Karolina Marszal <karomarszal@gmail.com>
2023-03-29 11:19:08 +0200

    Created MedicineTable

---
+++ src/components/MedicineTable.jsx
@@ -0,0 +1,30 @@
+import React from "react"
+
+const MedicineTable = () => {
+
+
+  return (
+    <div className="tableContainer">
+      <table className="appointmentsTable">
+        <thead>
+          <tr>
+            <th scope="col">Medicine</th>
+            <th scope="col">Placeholder</th>
+          </tr>
+        </thead>
+        <tbody>
+            <tr>
+              <td>
+                test
+              </td>
+              <td>
+                test
+              </td>
+            </tr>                
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default MedicineTable
