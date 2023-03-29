98fe03c01dad802867769f08f6ac5ab798a68690
Karolina Marszal <karomarszal@gmail.com>
2023-03-29 11:20:51 +0200

    Added medicine route

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,19 @@ import "./styles/_main.scss";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 //import pages
 import Home from "./pages/Home"
+import MedicinesPage from "./pages/MedicinesPage"
 //import components
 import Navbar from "./components/Navbar"
 
 
 function App() {
 
-
   return (
     <Router>
       <Navbar />
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route path="/medicines" element={<MedicinesPage />} />
       </Routes>
     </Router>
   )
