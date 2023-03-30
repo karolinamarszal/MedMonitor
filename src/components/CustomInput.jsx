270945e7cb00cdf36040487f59e719a916593730
Karolina Marszal <karomarszal@gmail.com>
2023-03-30 23:17:59 +0200

    Added class props

--- src/components/CustomInput.jsx
+++ src/components/CustomInput.jsx
@@ -26,46 +26,48 @@ const CustomInput = (props) => {
 
 
   return (
-    <div 
-      className={classNames({
-        "inputContainer inputContainerActive": isClicked,
-        "inputContainer": !isClicked,
-        "inputContainerFullWidth": !!props.fullWidth,
-      })}
-      onClick={handleClick}
-      onBlur={handleBlur}
-    >
-      {props.icon && <i 
-      className={classNames({
-        "iconForm": !isClicked,
-        "iconFormActive iconForm": isClicked,
-      })}
-      >
-        {props.icon}
-      </i>}
-      <label 
-        htmlFor={uId}
+    <div className={props.className}>
+      <div 
         className={classNames({
-          "labelBlurredWithValue": !!props.value && !isClicked,
-          "labelActive": isClicked,
-          "label": !isClicked,
-          "hidden": !props.value && !isClicked && props.type=== "date",
-        })} 
-      >
-        {props.label}
-      </label>
-      <input 
-        type={props.type} 
-        className={classNames({
-          "hidden": props.type === "file",
+          "inputContainer inputContainerActive": isClicked,
+          "inputContainer": !isClicked,
+          "inputContainerFullWidth": !!props.fullWidth,
         })}
-        id={uId} 
-        value={props.value} 
-        onChange={props.onChange} 
-        name={props.name}
-        ref={inputRef}
-        onFocus={handleInputOnFocus}
-      />
+        onClick={handleClick}
+        onBlur={handleBlur}
+      >
+        {props.icon && <i 
+        className={classNames({
+          "iconForm": !isClicked,
+          "iconFormActive iconForm": isClicked,
+        })}
+        >
+          {props.icon}
+        </i>}
+        <label 
+          htmlFor={uId}
+          className={classNames({
+            "labelBlurredWithValue": !!props.value && !isClicked,
+            "labelActive": isClicked,
+            "label": !isClicked,
+            "hidden": !props.value && !isClicked && props.type=== "date",
+          })} 
+        >
+          {props.label}
+        </label>
+        <input 
+          type={props.type} 
+          className={classNames({
+            "hidden": props.type === "file",
+          })}
+          id={uId} 
+          value={props.value} 
+          onChange={props.onChange} 
+          name={props.name}
+          ref={inputRef}
+          onFocus={handleInputOnFocus}
+        />
+      </div>
     </div>
   );
 };
