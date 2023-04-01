9b8551dbff95c5eeb235eabd8dc8197a26fd61b1
Karolina Marszal <karomarszal@gmail.com>
2023-04-01 22:22:31 +0200

    Changed src of logo picture

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "react-router-dom"
-import logo from "../../public/logo_medmonitor.png"
+import logo from "../assets/logo_medmonitor.png"
 
 const Navbar = () => {
   return (
