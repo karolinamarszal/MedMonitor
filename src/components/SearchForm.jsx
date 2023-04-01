e0c23cdf4864c132c69f5a8400ca3097c84131f8
Karolina Marszal <karomarszal@gmail.com>
2023-04-01 22:22:16 +0200

    Fetched data from API

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,34 +1,53 @@
-import React, { useState, useEffect} from "react"
-import { uniqueId } from "lodash";
+import React, { useContext, useState, useCallback} from "react"
 import { FaSearch } from "react-icons/fa";
-import classNames from "../utils/helpers";
 import CustomInput from "./CustomInput";
+import { MedicinesContext } from "../context/MedicinesContext";
+import { debounce } from "lodash";
 
 const SearchForm = () => {
 
-  const uId = uniqueId();
-  const [isFocused, setIsFocused] = useState(false);
-  const [search, setSearch] = useState('');
+  const [medicines, setMedicines] = useState([]);
+  const [searchedMedicines, setSearchedMedicines] = useState([]);
 
-  const handleChange = (e) => {
-    const {value} = e.target;
-    // const value = e.target.value;
-    setSearch(value)
-  };
+
+  const handleChange = debounce(async (value) => {
+    if(!value){
+      setSearchedMedicines([])
+      return
+    }
+    try {
+      const response = await fetch(`https://api.fda.gov/drug/drugsfda.json?search=products.brand_name:"${value}"&limit=100`);
+      const data = await response.json();
+      setSearchedMedicines(data.results.map((item)=>{
+        return item.products[0].brand_name;
+      }));
+    }
+    catch (err){
+      console.log(err)
+    }
+  }, 500);
+
+
 
   return (
     <section className="searchContainer">
-      <form className="searchForm">
-        <div className="formControl">
+      <form className="formControl">
           <CustomInput 
             label="Search Drug" 
             fullWidth={true}
             type="text" 
+            icon={<FaSearch />}
             name="searchDrug" 
-            value={search} 
-            onChange={handleChange}
+            onChange={e => handleChange(e.target.value)}
           />
-        </div>
+          {searchedMedicines.length > 0 && 
+          <ul>
+            {searchedMedicines.map((item, index)=>{
+              return (
+                <li key={index}>{item}</li>
+              )
+            })}
+          </ul>}
       </form>
     </section>
   )
