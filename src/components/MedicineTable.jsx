011bff576766e5cab102a0514dc548271728a641
Karolina Marszal <karomarszal@gmail.com>
2023-04-04 12:33:53 +0200

    Added searched drug to the table

--- src/components/MedicineTable.jsx
+++ src/components/MedicineTable.jsx
@@ -1,7 +1,14 @@
-import React from "react"
+import React, { useContext } from "react"
+import { MedicinesContext } from "../context/MedicinesContext";
+import { FaTrashAlt } from "react-icons/fa";
 
 const MedicineTable = () => {
 
+  const {medicines, setMedicines} = useContext(MedicinesContext);
+
+  const removeMedicine = (medicineIndex) => {
+    setMedicines(medicines.filter((medicine, index) => index !== medicineIndex))
+  }
 
   return (
     <div className="tableContainer">
@@ -9,18 +16,27 @@ const MedicineTable = () => {
         <thead>
           <tr>
             <th scope="col">Medicine</th>
-            <th scope="col">Placeholder</th>
+            <th scope="col">Active ingredients</th>
+            <th scope="col">Remove drug</th>
           </tr>
         </thead>
         <tbody>
-            <tr>
-              <td>
-                test
-              </td>
-              <td>
-                test
-              </td>
-            </tr>                
+          {medicines.length === 0 && <tr><td colSpan="3" className="tableCenter">No drugs yet</td></tr>}
+          {medicines.map((medicine, index) => {
+            return (
+              <tr key={index}>
+                <td>
+                  {medicine.name}
+                </td>
+                <td>
+                  {medicine.activeIngredients}
+                </td>
+                <td>
+                    <button type="button" className="deleteButton" onClick={()=>removeMedicine(index)}><FaTrashAlt /></button>
+                  </td>
+              </tr>
+            )
+          })}      
         </tbody>
       </table>
     </div>
