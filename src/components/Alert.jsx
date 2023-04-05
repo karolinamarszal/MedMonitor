29f43d0f86b1248e74b3224132559a6586b0f8e4
Karolina Marszal <karomarszal@gmail.com>
2023-04-05 19:25:12 +0200

    Removed dependency on appointments

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from "react"
 
-const Alert = ({type, msg, removeAlert, appointments}) => {
+const Alert = ({type, msg, removeAlert}) => {
   useEffect(()=> {
     const timeout = setTimeout(()=> {
       removeAlert()
     }, 3000)
     return () => clearTimeout(timeout)
-  }, [appointments])
+  }, [])
 
   return (
     <p className={`alert alert-${type}`}>{msg}</p>
