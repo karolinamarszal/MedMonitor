0ba182a578e7fcb29c2f70af16354d9b6fe279fe
Karolina Marszal <karomarszal@gmail.com>
2023-04-05 19:12:49 +0200

    Created new alert for medicines

---
+++ src/context/MedicinesContext.jsx
@@ -0,0 +1,40 @@
+import { debounce } from "lodash";
+import React, { useState, createContext, useContext, useMemo } from "react";
+
+
+
+export const MedicinesContext = createContext(null);
+
+export const MedicinesContextProvider = ({ children }) => {
+
+  const [medicines, setMedicines] = useState([]);
+  const [searchedMedicines, setSearchedMedicines] = useState([]);
+
+  const [alert, setAlert] = useState({
+    show: false,
+    msg: "",
+    type: "",
+  });
+
+  const showAlert = (show=false, type="", msg="")=>{
+    setAlert({show, type, msg})
+  }
+
+  const value = {
+    medicines,
+    setMedicines,
+    searchedMedicines,
+    setSearchedMedicines,
+    alert,
+    showAlert,
+  }
+
+  return (
+    <MedicinesContext.Provider value={value}>
+      { children }
+    </MedicinesContext.Provider>
+  );
+
+};
+
+export default MedicinesContext;
