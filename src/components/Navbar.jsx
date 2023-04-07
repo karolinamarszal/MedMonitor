95c69699ba05451b438174d594c15ebf3d51836a
Karolina Marszal <karomarszal@gmail.com>
2023-04-07 19:44:34 +0200

    Added active link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,22 @@
 import React from "react"
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 import logo from "../assets/logo_medmonitor.png"
 
 const Navbar = () => {
   return (
     <nav>
       <div className="navbar">
-        <Link to="/">
+        <NavLink className="navItem" to="/">
           <img src={logo} alt="medmonitor logo" className="logo"/>
           <h2 className="name">MEDMONITOR</h2>
-        </Link>
+        </NavLink>
         <div className="nav-links">
-          <Link to="/">
+          <NavLink className="navItem" to="/">
             <h2>home</h2>
-          </Link>
-          <Link to="/medicines">
+          </NavLink>
+          <NavLink className="navItem" to="/medicines">
             <h2>medicines</h2>
-          </Link>
+          </NavLink>
         </div>
       </div>
     </nav>
